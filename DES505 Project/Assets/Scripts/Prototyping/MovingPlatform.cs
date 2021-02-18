bd435eb8d049f8182bab01415b3757e4e73164bc
Sandy Steele <Sandy_steele@hotmail.com>
2021-02-18 14:42:12 +0000

    Added a couple of grey box revisions to the first area.

---
+++ DES505 Project/Assets/Scripts/Prototyping/MovingPlatform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject endMarker;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    public int transitionTime = 5;
+    private float currentTime;
+    public bool loop;
+    private bool returnJourney;
+    private bool stopped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = endMarker.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!stopped)
+        {
+            float t = currentTime / transitionTime;
+            transform.position = Vector3.Lerp(startPos, endPos, t);
+
+            if (currentTime >= transitionTime)
+            {
+                if (loop)
+                {
+                    returnJourney = true;
+                } else
+                {
+                    stopped = true;
+                }
+            }
+            
+            if (currentTime <= 0)
+            {
+                returnJourney = false;
+            }
+
+            if (!returnJourney)
+            {
+                currentTime += Time.deltaTime;
+            }
+            else
+            {
+                currentTime -= Time.deltaTime;
+            }
+        }
+    }
+}
---
+++ DES505 Project/Assets/Scripts/Prototyping/ProtoEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ProtoEnemy : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+
+}
