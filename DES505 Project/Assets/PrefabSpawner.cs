a995cc6ed943dfe221cfb1f20153fb1e8bfdb5a8
Sandy Steele <Sandy_steele@hotmail.com>
2021-03-15 13:04:56 +0000

    Box spawner and conveyor belt implementation
    Boxes do not delete, a trash collector must be implemented as well as a time to kill, I just wanted to push this first.

---
+++ DES505 Project/Assets/PrefabSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+    public float minTime;
+    public float maxTime;
+
+    public GameObject[] objects;
+    public bool canSpawn = false;
+    public bool startSpawn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startSpawn)
+        {
+            spawn();
+        }
+        else
+        {
+            StartCoroutine(coolDown());
+        }
+    }
+
+    public void spawn()
+    {
+        int index = Random.Range(0, objects.Length - 1);
+        GameObject spawnIn = objects[index];
+        GameObject.Instantiate(spawnIn, transform.position, Quaternion.Euler(spawnIn.transform.rotation.x, Random.Range(-180,180), spawnIn.transform.rotation.z));
+
+        StartCoroutine(coolDown());
+    }
+
+    IEnumerator coolDown()
+    {
+        yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+        spawn();
+    }
+}
---
+++ DES505 Project/Assets/Scripts/Art/Conveyor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Conveyor : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 pos = rb.position;
+        rb.position += transform.forward * speed * Time.fixedDeltaTime;
+        rb.MovePosition(pos);
+    }
+}
