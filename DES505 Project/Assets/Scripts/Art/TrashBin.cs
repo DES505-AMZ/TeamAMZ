f3333a8498fb42a50fe1db33d7f5994c64ce0860
Sandy Steele <Sandy_steele@hotmail.com>
2021-03-15 17:15:53 +0000

    Trash Can Implementation
    Implemented the trash cans to clean up the packages at the end of the conveyor lines.

---
+++ DES505 Project/Assets/Scripts/Art/TrashBin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour
+{
+    public string whatIDelete;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(whatIDelete))
+        {
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+}
