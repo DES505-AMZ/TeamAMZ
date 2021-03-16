764b19b0d7d0bd192ba49e6401ab73ab8d323084
Sandy Steele <Sandy_steele@hotmail.com>
2021-03-16 14:42:26 +0000

    Fixed minor issues, and added Companion Cube Spawn
    Fixed issues with the water bottle etc.

--- DES505 Project/Assets/Scripts/Art/PrefabSpawner.cs
+++ DES505 Project/Assets/Scripts/Art/PrefabSpawner.cs
@@ -8,11 +8,19 @@ public class PrefabSpawner : MonoBehaviour
     public float maxTime;
 
     public GameObject[] objects;
+    public GameObject[] rareObjects;
+
+    public float rareObjectPCT = 0.01f;
     public bool canSpawn = false;
     public bool startSpawn = true;
     // Start is called before the first frame update
     void Start()
     {
+        if(rareObjects.Length <= 0)
+        {
+            rareObjectPCT = -1f;
+        }
+
         if (startSpawn)
         {
             spawn();
@@ -26,7 +34,25 @@ public class PrefabSpawner : MonoBehaviour
     public void spawn()
     {
         int index = Random.Range(0, objects.Length - 1);
-        GameObject spawnIn = objects[index];
+        GameObject spawnIn;
+        if ((float) Random.Range(0f, 1.0f) <= rareObjectPCT)
+        {
+            if (rareObjects.Length == 1)
+            {
+                index = 0;
+            }
+            else
+            {
+                index = Random.Range(0, rareObjects.Length - 1);
+            }
+            Debug.Log("Error");
+            spawnIn = rareObjects[index];
+            Debug.Log("I've spawned a surprise!");
+        } else
+        {
+            spawnIn = objects[index];
+        }
+
         GameObject.Instantiate(spawnIn, transform.position, Quaternion.Euler(-90, Random.Range(-180,180), spawnIn.transform.rotation.z));
 
         StartCoroutine(coolDown());
