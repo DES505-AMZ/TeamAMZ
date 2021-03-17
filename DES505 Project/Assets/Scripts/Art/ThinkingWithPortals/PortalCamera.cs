c42740eb0fbdcf231c4703a9add39dc3ce6a7e2b
Sandy Steele <Sandy_steele@hotmail.com>
2021-03-17 00:11:07 +0000

    Added the non-euclidean cardboard box
    Cause, I always wanted to implement it.
    I promise I've wasted enough time on those boxes now.

---
+++ DES505 Project/Assets/Scripts/Art/ThinkingWithPortals/PortalCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+
+    public Transform entranceSurface;
+    public Transform exitSurface;
+    public Transform playerCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCam = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Matrix4x4 m = exitSurface.localToWorldMatrix * entranceSurface.worldToLocalMatrix * playerCam.localToWorldMatrix;
+        transform.SetPositionAndRotation(m.GetColumn(3), m.rotation);
+    }
+}
