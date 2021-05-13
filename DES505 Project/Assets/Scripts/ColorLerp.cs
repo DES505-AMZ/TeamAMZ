ca2c3bbcd44c11a898d892d1fd015abc6aa31a1f
Sandy Steele <Sandy_steele@hotmail.com>
2021-05-13 13:02:09 +0100

    Tweaked the start menu to display the name of the game.

---
+++ DES505 Project/Assets/Scripts/ColorLerp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorLerp : MonoBehaviour
+{
+    public Color startCol;
+    public Color endCol;
+
+    private Text thisText;
+
+    public int time;
+    private float timeSince;
+    public float prebake;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisText = GetComponent<Text>();
+        timeSince -= prebake;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeSince <= time)
+        {
+            float t = (float) timeSince / time;
+            thisText.color = Color.Lerp(startCol, endCol, t);
+            timeSince += Time.deltaTime;
+        }
+    }
+}
